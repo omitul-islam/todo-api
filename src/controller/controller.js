586730c505d22a81b8ef7dbd60a3a2684e802efb
omitul.islam <omitul.islam@brainstation-23.com>
2025-04-10 15:30:20 +0600

    methods in controller are added to CRUD operation

---
+++ src/controller/controller.js
@@ -0,0 +1,34 @@
+import { getTodos, createTodo, deleteTodo, updateTodo } from "../service/service";
+
+
+export const getTasks = (req, res)=> {
+     const todos = getTodos();
+     return todos;
+}
+
+export const addTask = (req, res) => {
+     const {task} = req.body;
+     const newTask = createTodo(task);
+
+    return res.status(201).json(newTask);
+}
+
+export const deleteTask = ()=>{
+    const id = req.param;
+    const deletedTask = deleteTodo(Number(id));
+    if(!deleteTask) {
+      return res.status(404).json({message: "Todo not found for this id!"});  
+    }
+    res.json({message: "This task is deleted!"}, deleteTask);      
+}
+
+export const EditTask = (req, res)=>{
+    const id = req.param;
+    const {modifiedTask} = req.body;
+    const updatedTask = updateTodo(Number(id), modifiedTask);
+    
+    if(!updatedTask) {
+      res.status(404).json({message:"No todo found for this id!"});  
+    }
+    res.json({message:"Todo is updated!"}, updatedTask);
+}

4ceec07c85f78ddcf9d42d2b90261c992bc58f50
omitul.islam <omitul.islam@brainstation-23.com>
2025-04-22 20:24:13 +0600

    bug fixed in login, now password and email will be verified before login

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -2,6 +2,7 @@ import userModel from "./auth.model.js";
 import { CreateService } from "./auth.service.js";
 import { validation } from "./auth.validate.js";
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 export const addUser = async (req, res, next) => {
       try {
@@ -34,11 +35,19 @@ export const loginUser = async (req, res, next) => {
     try {
         const { email } = req.body;
         console.log(email);
-        const user = await userModel.find({email:email});
+        const user = await userModel.findOne({email:email});
         console.log(user);
         if (!user || user.length === 0) {
             return res.status(401).json({ message: "Invalid email!"});
         }
+
+        const password = user.password;
+        const isCorrect = await bcrypt.compare(req.body.password, password);
+
+        if(!isCorrect) {
+            return res.status(401).json({ message: "Invalid password!"});
+        }
+        
         jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN }, (err, token) => {
             if (err) {
                 return res.status(500).json({ error: "Error signing token" });
